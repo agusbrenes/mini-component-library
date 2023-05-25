e23fe87ccff79b463f8986fa2931c17e36d19d87
Agustin Brenes <abrenes@konradgroup.com>
2023-05-25 08:51:46 -0600

    Adjust styles and remove unused component

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -8,18 +8,18 @@ import VisuallyHidden from "../VisuallyHidden";
 const SIZE_VARIANT_STYLES = {
 	small: {
 		"--border-radius": "4px",
-		"--spacing": 0,
+		"--padding": 0,
 		"--height": "8px",
 	},
 	medium: {
 		"--border-radius": "4px",
-		"--spacing": 0,
+		"--padding": 0,
 		"--height": "12px",
 	},
 	large: {
 		"--border-radius": "8px",
-		"--spacing": "0.25rem",
-		"--height": "16px",
+		"--padding": "0.25rem",
+		"--height": "24px",
 	},
 };
 
@@ -28,9 +28,14 @@ const ProgressBar = ({ value, size }) => {
 	const labelId = "progressBar";
 
 	return (
-		<Wrapper role="progressbar" aria-valuenow={value} style={variantStyle}>
-			<VisuallyHidden id={labelId}>{value}% complete</VisuallyHidden>
-			<Background />
+		<Wrapper
+			role="progressbar"
+			aria-valuemin={0}
+			aria-valuemax={100}
+			aria-valuenow={value}
+			style={variantStyle}
+		>
+			<VisuallyHidden id={labelId}>{value}%</VisuallyHidden>
 			<BarContainer>
 				<Bar value={value} aria-labelledby={labelId} />
 			</BarContainer>
@@ -39,27 +44,17 @@ const ProgressBar = ({ value, size }) => {
 };
 
 const Wrapper = styled.div`
-	position: relative;
-	height: var(--height);
-	isolation: isolate;
-`;
-
-const Background = styled.div`
-	position: absolute;
-	box-sizing: content-box;
 	box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
 	border-radius: var(--border-radius);
-	padding: var(--spacing);
+	padding: var(--padding);
 	width: 100%;
-	height: 100%;
+	height: var(--height);
 	background-color: ${COLORS.transparentGray15};
 `;
 
 const BarContainer = styled.div`
-	position: absolute;
 	overflow: clip;
 	border-radius: 4px;
-	margin: var(--spacing);
 	width: 100%;
 	height: 100%;
 `;
